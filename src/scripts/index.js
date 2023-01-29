3fc02d78157cadc9bf06b8b9164cca3540ea0fcc
Bulat Sitdikov <rdvad2@gmail.com>
2023-01-29 14:07:39 +0300

    fix navbar adaptive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-import {takeControlBurger, takeControlDirections, takeControlMap, takeControlAccordion} from './takeControl'
+import { takeControlBurger, takeControlDirections, takeControlMap, takeControlAccordion } from "./takeControl";
 
 takeControlBurger();
 takeControlDirections();
@@ -10,6 +10,7 @@ const newsSwiper = new Swiper(".news-swiper", {
 	pagination: {
 		el: ".swiper-pagination",
 	},
+  clickable: true,
 	mousewheel: false,
 	keyboard: true,
 });
@@ -18,22 +19,28 @@ const projectSwiper = new Swiper(".project__swiper", {
 	direction: "horizontal",
 	slidesPerView: 4,
 	spaceBetween: 30,
-	pagination: {
-		el: ".swiper-pagination",
-	},
 	mousewheel: false,
 	keyboard: true,
 	breakpoints: {
 		360: {
 			slidesPerView: 1,
+			pagination: {
+				el: ".swiper-pagination",
+			},
 		},
 		690: {
 			slidesPerView: 2,
+			pagination: {
+				el: ".swiper-pagination",
+			},
 		},
 		1020: {
 			slidesPerView: 3,
+			pagination: {
+				el: ".swiper-pagination",
+			},
 		},
-		1350: {
+		1351: {
 			direction: "vertical",
 		},
 	},
@@ -43,7 +50,8 @@ const navSwiper = new Swiper(".nav-bar", {
 	slidesPerView: "auto",
 	spaceBetween: 24,
 	navigation: {
-		nextEl: '.swiper-button-next',
+		prevEl: ".swiper-button-prev",
+		nextEl: ".swiper-button-next",
 	},
 	mousewheel: false,
 	keyboard: true,
@@ -54,11 +62,19 @@ const navSwiper = new Swiper(".nav-bar", {
 	},
 });
 
-takeControlAccordion('.about-company','.accordion__list', {
-	accordionItemSelector: '.accordion__item',
-	accordionItemTogglerSelector: '.accordion__toggle',
-	accordionItemContentSelector: '.accordion__content',
-	accordionItemActiveClass: 'accordion__item_active'
+takeControlAccordion(".about-company", ".accordion__list", {
+	accordionItemSelector: ".accordion__item",
+	accordionItemTogglerSelector: ".accordion__toggle",
+	accordionItemContentSelector: ".accordion__content",
+	accordionItemActiveClass: "accordion__item_active",
 });
 
+const headerNavLink = document.querySelectorAll(".header__nav-link");
+headerNavLink.forEach((el) => {
+	if (el.getAttribute("href") == window.location.pathname) el.classList.add("header__nav-link_active");
+});
 
+const navBarLink = document.querySelectorAll(".nav-bar__link");
+navBarLink.forEach((el) => {
+	if (el.getAttribute("href") == window.location.pathname) el.classList.add("nav-bar__link_active");
+});
