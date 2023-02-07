e0ee6fd4115d6b2b435a6a0db5a82a6d94c4503e
Bulat Sitdikov <rdvad2@gmail.com>
2023-02-07 17:59:19 +0300

    bugs fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -110,8 +110,8 @@ const qualitySwiper = new Swiper(".quality__swiper", {
 		991: {
 			direction: "vertical",
 			slidesPerView: 3,
-		}
-	}
+		},
+	},
 });
 
 takeControlAccordion(".about-company", ".accordion__list", {
@@ -135,121 +135,135 @@ takeControlAccordion(".direction", ".accordion__list", {
 	accordionItemActiveClass: "accordion__item_active",
 });
 
+// const fileInput = document.querySelector(".input-file-container");
+// fileInput.children[0].insertAdjacentHTML(
+// 	"afterend",
+// 	'<img src="./files/icons/clip.svg"><div class="file-button">Прикрепить файл</div><span class="file-name"></span>'
+// );
+// fileInput.children[0].addEventListener("change", function () {
+// 	const splitFileName = this.value.split(/\\/);
+// 	fileInput.children[3].innerText = splitFileName[splitFileName.length - 1];
+// });
 
+// const successModal = document.getElementById("success-modal");
+// const form = document.getElementById("form");
+// const closeModalButton = document.querySelector(".modal-close");
+// form.addEventListener("submit", (event) => {
+// 	event.preventDefault();
+// 	successModal.classList.add("modal_active");
+// 	event.target.reset();
+// });
+// closeModalButton.addEventListener("click", () => {
+// 	successModal.classList.remove("modal_active");
+// });
 
-// const paginationNumbers = document.getElementById("pagination-numbers");
-const paginationNumbers = document.querySelector(".pagination-numbers");
-// const paginatedList = document.getElementById("paginated-list");
-const paginatedList = document.querySelector(".news-tabs__page");
-const listItems = paginatedList.querySelectorAll("li");
-const nextButton = document.getElementById("next-button");
-const prevButton = document.getElementById("prev-button");
+// // const paginationNumbers = document.getElementById("pagination-numbers");
+// const paginationNumbers = document.querySelector(".pagination-numbers");
+// // const paginatedList = document.getElementById("paginated-list");
+// const paginatedList = document.querySelector(".news-tabs__page");
+// const listItems = paginatedList.querySelectorAll("li");
+// const nextButton = document.getElementById("next-button");
+// const prevButton = document.getElementById("prev-button");
 
-const paginationLimit = 4;
-const pageCount = Math.ceil(listItems.length / paginationLimit);
-let currentPage = 1;
+// const paginationLimit = 4;
+// const pageCount = Math.ceil(listItems.length / paginationLimit);
+// let currentPage = 1;
 
-const disableButton = (button) => {
-  button.classList.add("disabled");
-  button.setAttribute("disabled", true);
-};
+// const disableButton = (button) => {
+//   button.classList.add("disabled");
+//   button.setAttribute("disabled", true);
+// };
 
-const enableButton = (button) => {
-  button.classList.remove("disabled");
-  button.removeAttribute("disabled");
-};
+// const enableButton = (button) => {
+//   button.classList.remove("disabled");
+//   button.removeAttribute("disabled");
+// };
 
-const handlePageButtonsStatus = () => {
-  if (currentPage === 1) {
-    disableButton(prevButton);
-  } else {
-    enableButton(prevButton);
-  }
+// const handlePageButtonsStatus = () => {
+//   if (currentPage === 1) {
+//     disableButton(prevButton);
+//   } else {
+//     enableButton(prevButton);
+//   }
 
-  if (pageCount === currentPage) {
-    disableButton(nextButton);
-  } else {
-    enableButton(nextButton);
-  }
-};
+//   if (pageCount === currentPage) {
+//     disableButton(nextButton);
+//   } else {
+//     enableButton(nextButton);
+//   }
+// };
 
-const handleActivePageNumber = () => {
-  document.querySelectorAll(".pagination-number").forEach((button) => {
-    button.classList.remove("active");
-    const pageIndex = Number(button.getAttribute("page-index"));
-    if (pageIndex == currentPage) {
-      button.classList.add("active");
-    }
-  });
-};
+// const handleActivePageNumber = () => {
+//   document.querySelectorAll(".pagination-number").forEach((button) => {
+//     button.classList.remove("active");
+//     const pageIndex = Number(button.getAttribute("page-index"));
+//     if (pageIndex == currentPage) {
+//       button.classList.add("active");
+//     }
+//   });
+// };
 
-const appendPageNumber = (index) => {
-  const pageNumber = document.createElement("button");
-  pageNumber.className = "pagination-number";
-  pageNumber.innerHTML = index;
-  pageNumber.setAttribute("page-index", index);
-  pageNumber.setAttribute("aria-label", "Page " + index);
+// const appendPageNumber = (index) => {
+//   const pageNumber = document.createElement("button");
+//   pageNumber.className = "pagination-number";
+//   pageNumber.innerHTML = index;
+//   pageNumber.setAttribute("page-index", index);
+//   pageNumber.setAttribute("aria-label", "Page " + index);
 
-  paginationNumbers.appendChild(pageNumber);
-};
+//   paginationNumbers.appendChild(pageNumber);
+// };
 
-const getPaginationNumbers = () => {
-  for (let i = 1; i <= pageCount; i++) {
-    appendPageNumber(i);
-  }
-};
+// const getPaginationNumbers = () => {
+//   for (let i = 1; i <= pageCount; i++) {
+//     appendPageNumber(i);
+//   }
+// };
 
-const setCurrentPage = (pageNum) => {
-  currentPage = pageNum;
+// const setCurrentPage = (pageNum) => {
+//   currentPage = pageNum;
 
-  handleActivePageNumber();
-  handlePageButtonsStatus();
+//   handleActivePageNumber();
+//   handlePageButtonsStatus();
 
-  const prevRange = (pageNum - 1) * paginationLimit;
-  const currRange = pageNum * paginationLimit;
+//   const prevRange = (pageNum - 1) * paginationLimit;
+//   const currRange = pageNum * paginationLimit;
 
-  listItems.forEach((item, index) => {
-    item.classList.add("hidden");
-    if (index >= prevRange && index < currRange) {
-      item.classList.remove("hidden");
-    }
-  });
-};
+//   listItems.forEach((item, index) => {
+//     item.classList.add("hidden");
+//     if (index >= prevRange && index < currRange) {
+//       item.classList.remove("hidden");
+//     }
+//   });
+// };
 
-const tabs = document.querySelectorAll(".news-tabs__content");
-tabs.forEach((el) => {
+// const tabs = document.querySelectorAll(".news-tabs__content");
+// tabs.forEach((el) => {
 
-})
-window.addEventListener("load", () => {
-  getPaginationNumbers();
-  setCurrentPage(1);
+// })
+// window.addEventListener("load", () => {
+//   getPaginationNumbers();
+//   setCurrentPage(1);
 
-  prevButton.addEventListener("click", () => {
-    setCurrentPage(currentPage - 1);
-  });
+//   prevButton.addEventListener("click", () => {
+//     setCurrentPage(currentPage - 1);
+//   });
 
-  nextButton.addEventListener("click", () => {
-    setCurrentPage(currentPage + 1);
-  });
+//   nextButton.addEventListener("click", () => {
+//     setCurrentPage(currentPage + 1);
+//   });
 
-  document.querySelectorAll(".pagination-number").forEach((button) => {
-    const pageIndex = Number(button.getAttribute("page-index"));
+//   document.querySelectorAll(".pagination-number").forEach((button) => {
+//     const pageIndex = Number(button.getAttribute("page-index"));
 
-    if (pageIndex) {
-      button.addEventListener("click", () => {
-        setCurrentPage(pageIndex);
-      });
-    }
-  });
-});
+//     if (pageIndex) {
+//       button.addEventListener("click", () => {
+//         setCurrentPage(pageIndex);
+//       });
+//     }
+//   });
+// });
 
-
-const headerNavLink = document.querySelectorAll(".header__nav-link");
-headerNavLink.forEach((el) => {
-	if (el.getAttribute("href") == window.location.pathname) el.classList.add("header__nav-link_active");
-});
-
-const navBarLink = document.querySelectorAll(".nav-bar__link");
-navBarLink.forEach((el) => {
-	if (el.getAttribute("href") == window.location.pathname) el.classList.add("nav-bar__link_active");
+// cookie notice
+document.getElementById("cookie_close").addEventListener("click", () => {
+	document.querySelector(".cookie").style.display = "none";
 });
