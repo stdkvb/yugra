03f3efddd2e3fe9f63e156cae60f220dfb32b32f
Bulat Sitdikov <b.sitdikov@wptt.ru>
2023-02-28 17:19:09 +0300

    ref accordion on main

--- src/scripts/controllers/directions-on-main/index.js
+++ src/scripts/controllers/directions-on-main/index.js
@@ -6,6 +6,26 @@ const takeControlDirections = () => {
 		});
 	};
 	if (document.getElementById("direction-1") !== null) {
+		document.getElementById("direction-1").addEventListener("mouseover", () => {
+			removeBackground();
+			document.getElementById("direction-background-1").classList.add("directions__background-image_active");
+		});
+
+		document.getElementById("direction-2").addEventListener("mouseover", () => {
+			removeBackground();
+			document.getElementById("direction-background-2").classList.add("directions__background-image_active");
+		});
+
+		document.getElementById("direction-3").addEventListener("mouseover", () => {
+			removeBackground();
+			document.getElementById("direction-background-3").classList.add("directions__background-image_active");
+		});
+
+		document.getElementById("direction-4").addEventListener("mouseover", () => {
+			removeBackground();
+			document.getElementById("direction-background-4").classList.add("directions__background-image_active");
+		});
+
 		document.getElementById("direction-1").addEventListener("click", () => {
 			removeBackground();
 			document.getElementById("direction-background-1").classList.add("directions__background-image_active");
@@ -28,6 +48,46 @@ const takeControlDirections = () => {
 
 		const directions = document.querySelectorAll(".directions__header");
 		directions.forEach((elem) => {
+			elem.addEventListener("mouseover", function () {
+				const aboutActive = document.querySelectorAll(".directions__about_active");
+				aboutActive.forEach((elem) => {
+					elem.classList.remove("directions__about_active");
+				});
+				const directionArrowActive = document.querySelectorAll(".directions__direction-button_active");
+				directionArrowActive.forEach((elem) => {
+					elem.classList.remove("directions__direction-button_active");
+				});
+				const titleActive = document.querySelectorAll(".directions__direction-title_active");
+				titleActive.forEach((elem) => {
+					elem.classList.remove("directions__direction-title_active");
+				});
+				const directionActive = document.querySelectorAll(".directions__direction_active");
+				directionActive.forEach((elem) => {
+					elem.classList.remove("directions__direction_active");
+				});
+				// const backgroundActive = document.querySelectorAll(".directions__background-image_active");
+				// backgroundActive.forEach((elem) => {
+				// 	elem.classList.remove("directions__background-image_active");
+				// });
+				const direction = this.parentNode;
+				const about = direction.querySelector(".directions__about");
+				const directionArrow = direction.querySelector(".directions__direction-button");
+				const title = direction.querySelector(".directions__direction-title");
+				// const background = direction.querySelector(".directions__background-image");
+				if (about.classList.contains("directions__about_active")) {
+					about.classList.remove("directions__about_active");
+					directionArrow.classList.remove("directions__direction-button_active");
+					title.classList.remove("directions__direction-title_active");
+					direction.classList.remove("directions__direction_active");
+					// background.classList.remove("directions__background-image_active");
+				} else {
+					about.classList.add("directions__about_active");
+					directionArrow.classList.add("directions__direction-button_active");
+					title.classList.add("directions__direction-title_active");
+					direction.classList.add("directions__direction_active");
+					// background.classList.add("directions__background-image_active");
+				}
+			});
 			elem.addEventListener("click", function () {
 				const aboutActive = document.querySelectorAll(".directions__about_active");
 				aboutActive.forEach((elem) => {
